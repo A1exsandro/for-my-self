42c7097ca8066f98e9caef45830a63eee3e44ba1
A1exsandro <alexsandro.acp@gmail.com>
2022-08-04 18:31:38 -0400

    first task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todo from "./Pages/Todo";
+import './App.css';
 
 function App() {
   return (
--- src/Pages/Todo/index.js
+++ src/Pages/Todo/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles.css';
 
 const Todo = () => {
+  const [todo, setTodo] = useState(['First Task']);
+
   return (
-    <div>
-      Welcome Todo! Right?
+    <div className='container'>
+      <div className='header'>
+        MY TASKS
+      </div>
+      <div className='task'>
+        {todo}
+      </div>
     </div>
   );
 }
