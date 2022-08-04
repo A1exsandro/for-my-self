14878b2ea6b145b16418f739907fd0b04a1c6648
A1exsandro <alexsandro.acp@gmail.com>
2022-08-04 18:40:47 -0400

    add react-hook-form

--- src/Pages/Todo/index.js
+++ src/Pages/Todo/index.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
 import './styles.css';
 
 const Todo = () => {
   const [todo, setTodo] = useState(['First Task']);
 
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
-    <div className='container'>
-      <div className='header'>
-        MY TASKS
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input defaultValue="test" {...register("task")} />
+        <input type="submit" />
+      </form>
+
+      <div className='container'>
+        <div className='header'>
+          MY TASKS
+        </div>
+        <div className='task'>
+          {todo} 
+        </div>
       </div>
-      <div className='task'>
-        {todo}
-      </div>
-    </div>
+    </>
   );
 }
 
