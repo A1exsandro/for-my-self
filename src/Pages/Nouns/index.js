224eeb52555d632671f3baf6f899c9250f3dfcfd
A1exsandro <alexsandro.acp@gmail.com>
2022-08-09 18:38:42 -0400

    noun grid

--- src/Pages/Nouns/index.js
+++ src/Pages/Nouns/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import './styles.css';
 
 const Nouns = () => {
+  const [todoList, setTodoList] = useState(['First Task']);
+  
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => setTodoList((previous) => [...previous, data.task]);
+
   return (
-    <div>
-      There is Nouns
+    <div className='nouns'>
+      <div className='formNoun'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input className='in_put' defaultValue="test" {...register("task")} />
+          <input className='button' type="submit" />
+        </form>
+        <div className='info'>There is { todoList.length }</div>
+      </div>
+      
+      <div className='contain'>
+        {
+          todoList.map((todo, index) => (
+            <div key={index} className='noun'>
+              {todo}
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
